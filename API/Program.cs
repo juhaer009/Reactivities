727dc8ddd4cda59336f21cc660e9f0d7b1cf9138
juhaer009 <juhaerhakim@gmail.com>
2026-08-13 18:07:53 +0600

    Section 4 Lesson 30

--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using Application;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -6,17 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext> (opt=>
+builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 builder.Services.AddCors();
+builder.Services.AddMediatR(x => 
+x.RegisterServicesFromAssemblyContaining<GetActivityList.Handler>());
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseCors(x=> x.AllowAnyHeader().AllowAnyMethod()
-    .WithOrigins("http://localhost:3000","https://localhost:3000"));
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod()
+    .WithOrigins("http://localhost:3000", "https://localhost:3000"));
 app.MapControllers();
 
 using var scope = app.Services.CreateScope();
@@ -29,7 +32,7 @@ try
     await DbInitializer.SeedData(context);
 }
 catch (Exception ex)
-{ 
+{
     var logger = services.GetRequiredService<ILogger<Program>>();
     logger.LogError(ex, "An error occured during migration.");
 }
